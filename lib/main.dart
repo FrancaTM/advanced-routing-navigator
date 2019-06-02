4658e3440b2a915430ba7aea7fbbd32e424d53b6
Túlio <francatm@outlook.com>
2019-06-02 11:57:46 -0300

    add named routing

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(),
+//      home: SecondHome(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => HomePage(),
+        '/second': (context) => SecondHome(),
+      },
+      onUnknownRoute: (RouteSettings settings) {
+        String unknownRoute = settings.name;
+        print(unknownRoute);
+        return MaterialPageRoute(builder: (context) {
+          return NotFoundPage();
+        });
+      },
     ),
   );
 }
@@ -19,10 +31,11 @@ class HomePage extends StatelessWidget {
         child: RaisedButton(
           child: Text('Second home'),
           onPressed: () {
-            Route route = MaterialPageRoute(builder: (context) {
-              return SecondHome();
-            });
-            Navigator.push(context, route);
+//            Route route = MaterialPageRoute(builder: (context) {
+//              return SecondHome();
+//            });
+//            Navigator.push(context, route);
+            Navigator.pushNamed(context, '/second');
           },
         ),
       ),
@@ -42,8 +55,23 @@ class SecondHome extends StatelessWidget {
           child: Text('go back'),
           onPressed: () {
             Navigator.pop(context);
+//            Navigator.pushNamed(context, '/404');
           },
         ),
+      ),
+    );
+  }
+}
+
+class NotFoundPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Route Not Found'),
+      ),
+      body: Center(
+        child: Text('404'),
       ),
     );
   }
